e1f25934cd8b07e8680fafab9c8dddc7030e1405
RoustyTousty <emils.misisons@gmail.com>
2024-03-12 22:20:38 +0200

    Added country names

---
+++ maps/deleteFiles.py
@@ -0,0 +1,42 @@
+# DELETE SPECIFIC FILES
+# import os
+
+# base_dir = ""
+
+# for root, dirs, files in os.walk(base_dir):
+#     for file in files:
+
+#         if file != "1024.png":
+
+#             file_path = os.path.join(root, file)
+
+#             os.remove(file_path)
+#             print(f"Deleted: {file_path}")
+
+
+
+
+# RENAME COUNTRIES
+# import os
+# import pycountry
+
+# def rename_folders(path):
+#     folders = os.listdir(path)
+    
+#     for folder in folders:
+#         folder_path = os.path.join(path, folder)
+        
+#         if os.path.isdir(folder_path):
+#             try:
+#                 country = pycountry.countries.get(alpha_2=folder.upper())
+#                 full_country_name = country.name
+#             except AttributeError:
+#                 print(f"Cannot find country name for code: {folder}")
+#                 continue
+            
+#             os.rename(folder_path, os.path.join(path, full_country_name))
+#             print(f"Renamed {folder} to {full_country_name}")
+
+# folder_path = "C:/Users/Lietotajs/Documents/GitHub/geoscrabble/maps/samerica"
+
+# rename_folders(folder_path)
